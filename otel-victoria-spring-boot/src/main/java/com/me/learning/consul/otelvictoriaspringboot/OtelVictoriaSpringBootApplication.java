91ac271bd031839804a26f3f541ea69e2667c0d3
ramu <test@test.com>
2025-07-08 22:07:34 +0400

    test

---
+++ otel-victoria-spring-boot/src/main/java/com/me/learning/consul/otelvictoriaspringboot/OtelVictoriaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.me.learning.consul.otelvictoriaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtelVictoriaSpringBootApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run (OtelVictoriaSpringBootApplication.class, args);
+    }
+
+}
